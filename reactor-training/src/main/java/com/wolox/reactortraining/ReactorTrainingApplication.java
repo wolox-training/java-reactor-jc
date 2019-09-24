592484638a833ef7901b7c2ad1a771a1fcb93541
Jhovanny Canas <jhovanycanas@Woloxs-MacBook-Pro.local>
2019-09-24 14:44:36 -0500

    Project Configuration

---
+++ reactor-training/src/main/java/com/wolox/reactortraining/ReactorTrainingApplication.java
@@ -0,0 +1,13 @@
+package com.wolox.reactortraining;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReactorTrainingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReactorTrainingApplication.class, args);
+	}
+
+}
